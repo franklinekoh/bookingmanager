ec56029531f63557ca774330186547bbf279be4a
marist4545 <kachi.scodu@gmail.com>
2019-07-04 20:33:51 +0100

    Added Booking and Room Seeders

---
+++ database/seeds/BookingsTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Repositories\BookingRepositoryInterface;
+use Faker\Factory as Faker;
+
+class BookingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    /**
+     * @var BookingRepositoryInterface
+     */
+    protected $booking;
+
+    /**
+     * BookingsTableSeeder constructor.
+     * @param BookingRepositoryInterface $booking
+     */
+    public function __construct(BookingRepositoryInterface $booking)
+    {
+        $this->booking = $booking;
+    }
+
+    public function run()
+    {
+        //
+        $faker = Faker::create();
+        for($i = 0; $i < 10; $i++){
+            $this->booking->store([
+                'room_id' => $i,
+                'start_date' => $faker->dateTime(),
+                'end_date' => $faker->dateTime(),
+                'customer_fullname' => $faker->name,
+                'customer_email' => $faker->email,
+                'total_nights' => $faker->numberBetween(1, 100),
+                'total_price' => $faker->numberBetween(1000, 1000000)
+            ]);
+        }
+
+    }
+}
